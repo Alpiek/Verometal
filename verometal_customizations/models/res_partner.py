4e1f2bdfed0d79235f16d74513b6c67553dee3a7
Przemek Kaminski <cgenie@gmail.com>
2018-07-06 14:35:33 +0100

    DEV-3079 verometal_customizations: add x_typology relation for res.partner

--- verometal_customizations/models/res_partner.py
+++ verometal_customizations/models/res_partner.py
@@ -1,4 +1,13 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+
+
+class Typology(models.Model):
+    _name = 'x_typology'
+
+    name = fields.Char(
+        string=_('Name'),
+        required=True
+    )
 
 
 class ResPartner(models.Model):
@@ -6,11 +15,15 @@ class ResPartner(models.Model):
 
     v_parent_id = fields.Many2one(
         'res.partner',
-        string='Business Partner',
+        string=_('Business Partner'),
         required=False
     )
     v_child_ids = fields.One2many(
         'res.partner',
         'v_parent_id',
-        string='Link Businesses'
+        string=_('Link Businesses')
+    )
+    x_typology = fields.Many2many(
+        'x_typology',
+        string=_('Typology')
     )
