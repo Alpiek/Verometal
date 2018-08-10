c180bf7e665234cbd7929d6b65b2c6c8a8414d06
Przemek Kaminski <cgenie@gmail.com>
2018-08-10 07:18:17 +0100

    DEV-3624 adr_chemicals module added

---
+++ adr_chemicals/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Module for ADR - hazardous chemicals',
+    'version': '11.0.0.1.0.0',
+    'description': """
+    ADR module.
+    """,
+    'author': "Alpiek",
+    'website': "https://alpiek.nl/",
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'application': False,
+    'license': u'OPL-1',
+}
